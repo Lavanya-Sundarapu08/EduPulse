76236166a2ab85f15f9d1d2e9f71d8c537f5c5f8
Lavanya Sundarapu <lavanyadevika1122@gmail.com>
2026-04-10 08:49:18 +0530

    sample test added

---
+++ src/test/java/com/edupulse/SampleTest.java
@@ -0,0 +1,12 @@
+package com.edupulse;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SampleTest {
+
+    @Test
+    void testAddition() {
+        assertEquals(4, 2 + 2);
+    }
+}
